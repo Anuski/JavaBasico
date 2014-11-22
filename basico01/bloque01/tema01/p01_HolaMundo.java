046b7fd30fabcb442e9ff26832e2bdb5c8d13331
Anuski <anuski.web@gmail.com>
2014-11-22 11:52:55 +0100

    HolaMundo con algún comentario

---
+++ basico01/bloque01/tema01/p01_HolaMundo.java
@@ -0,0 +1,19 @@
+package tema;
+
+public class p01_HolaMundo {
+	// Declaración delos atributos de la clase
+
+	// Declaración de los métodos de una clase
+
+	// El método main, que indica donde empieza la ejecución
+
+	public static void main(String[] args) {
+
+		// Declaración de las variables del método
+
+		// Sentencias de ejecución del método
+		System.out.println("Hola a todos!");
+
+	}
+
+}
