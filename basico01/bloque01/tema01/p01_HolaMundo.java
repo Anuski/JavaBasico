7c52763c159721d2130c5212a0c6307c226f7368
Anuski <anuski.web@gmail.com>
2014-11-22 11:55:28 +0100

    Update p01_HolaMundo.java

--- basico01/bloque01/tema01/p01_HolaMundo.java
+++ basico01/bloque01/tema01/p01_HolaMundo.java
@@ -1,6 +1,26 @@
 package tema;
 
 public class p01_HolaMundo {
+	//"p01_HolaMundo" es el nombre de la clase
+
+	// Declaración delos atributos de la clase
+
+	// Declaración de los métodos de una clase
+
+	// El método main, que indica donde empieza la ejecución
+
+	public static void main(String[] args) {
+		//"main" es el metodo o funcion
+		//"String[]" es el parámetro
+
+		// Declaración de las variables del método
+
+		// Sentencias de ejecución del método
+		System.out.println("Hola a todos!");
+
+	}
+
+}
 	// Declaración delos atributos de la clase
 
 	// Declaración de los métodos de una clase
