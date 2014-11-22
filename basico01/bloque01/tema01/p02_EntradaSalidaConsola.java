f3a1245405c2570bc71505a2154696627fb1a854
Anuski <anuski.web@gmail.com>
2014-11-22 13:09:13 +0100

    Añadimos diferentes scanner
    Añadimos diferentes scanner para diferentes tipos de datos

--- basico01/bloque01/tema01/p02_EntradaSalidaConsola.java
+++ basico01/bloque01/tema01/p02_EntradaSalidaConsola.java
@@ -6,19 +6,27 @@ public class p02_EntradaSalidaConsola {
 
 	public static void main(String[] args) {
 		
-		//Para leer datos por teclado usamos la clase Scanner
+		// Para leer datos por teclado usamos la clase Scanner
 		Scanner scanner = new Scanner(System.in);
-		
-		//Pedimos al usuario que escriba su nombre
-		//Se utiliza println si queremos que añada un salto de linea
-		//Print sin salto de linea
+
+		// Pedimos al usuario que escriba su nombre
+		// Se utiliza println si queremos que añada un salto de linea
+		// Print sin salto de linea
 		System.out.println("Escriba su nombre: ");
-		
-		//Leemos el valor metido por teclado y se lo damos a "nombre"
-		String nombre = scanner.nextLine();
-		
-		//Lo sacamos por pantalla
-		System.out.println("Hola " + nombre);
+
+		// Leemos el valor metido por teclado y se lo damos a "nombre"
+		String nombre = scanner.nextLine(); // Es un String
+
+		System.out.println("Escriba su edad: ");
+		int edad = scanner.nextInt(); // Es un entero
+
+		System.out.println("Escriba su estatura (con , para los decimales): ");
+		double estatura = scanner.nextDouble(); // Es un double
+
+		// Lo sacamos por pantalla
+		System.out.println("Hola " + nombre + "!");
+		System.out.println("Tienes " + edad + "años y mides " + estatura);
+
 
 	}
 
